5dbfac63e771931513a8ae158f658b918a9ba2e1
Leandro Lino <leao.lino7@gmail.com>
2022-02-22 19:31:29 -0300

    Portfolio :: Added :: Easter Egg

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import SideDrawer from "./components/SideDrawer";
 import Home from "./pages/Home";
 
 function App() {
+  console.error(
+    "%c O que você busca por aqui?",
+    "font-size: 50px; text-transform: uppercase; color: white; text-shadow: 2px 2px red, 4px 4px orange, 6px 6px yellow, 8px 8px green, 10px 10px blue, 12px 12px purple;"
+  );
   return (
     <div>
       <Header />
