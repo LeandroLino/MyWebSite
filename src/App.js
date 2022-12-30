8016beed101fa6f51abcfcd71eb5352840851b0b
LeandroLino <leao.lino7@gmail.com>
2022-12-30 11:58:34 -0300

    Removed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import GetLocation from "./components/GetLocation";
 import Header from "./components/Header";
 import SideDrawer from "./components/SideDrawer";
 import Home from "./pages/Home";
-import Amor from "./pages/Amor";
 import { Route, Routes, BrowserRouter as Router, useLocation } from "react-router-dom";
 import { useState } from "react";
 
@@ -12,14 +11,12 @@ function App() {
     <div>
       <Router>
       <GetLocation setLocation={setLocation}/>
-        {location.pathname != "/amor" && 
           <>
             <Header />
             <SideDrawer />
-          </>}
+          </>
         <Routes>
           <Route exact path="/" element={<Home />} />
-          <Route exact path="/amor" element={<Amor />} />
         </Routes>
       </Router>
     </div>
